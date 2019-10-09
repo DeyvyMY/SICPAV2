262a5126bb6be2d95daf48d36117a8e8c7fffd96
michael <michael_h_p_@hotmail.com>
2019-10-09 16:45:54 -0500

    usan BaseController

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Categories;
+use App\Utils\BaseController;
 use const http\Client\Curl\Features\HTTP2;
 use http\Env\Response;
 use Illuminate\Http\Request;
@@ -12,7 +13,7 @@ use Illuminate\View\View;
 use Illuminate\Http\JsonResponse;
 use Symfony\Component\Translation\Exception\NotFoundResourceException;
 
-class CategoriesController extends Controller
+class CategoriesController extends BaseController
 {
     /**
      *
@@ -36,111 +37,10 @@ class CategoriesController extends Controller
         return view("categories.index", compact("categories"));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
+    public function __construct()
     {
-        //
+        parent::__construct(Categories::class);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param \Illuminate\Http\Request $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
 
-        try {
-            $category = new Categories();
-            $category->validateAndFill($request->all());
-            $category->save();
-        } catch (ValidationException $exception) {
-
-            return response()->json(
-                ["errors" => $exception->validator->getMessageBag()],
-                JsonResponse::HTTP_UNPROCESSABLE_ENTITY);
-        }
-        return response()->json(["Category" => $category],
-            JsonResponse::HTTP_CREATED);
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param \App\Categories $categories
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Categories $categories)
-    {
-        //
-        return new \Illuminate\Http\Response("FFFFF");
-
-
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param \App\Categories $categories
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Categories $categories)
-    {
-        // GET           /users/{user}/edit          edit    users.edit
-        //return view("categories.editForm");
-        return response()->view("categories.editForm");
-
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param \Illuminate\Http\Request $request
-     * @param \App\Categories $categories
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        try {
-            $categories=Categories::find($id);
-            $categories->validateAndFill($request->all());
-            $categories->save();
-        } catch (ValidationException $exception) {
-
-            return response()->json(
-                ["errors" => $exception->validator->getMessageBag()],
-                JsonResponse::HTTP_UNPROCESSABLE_ENTITY);
-        }
-        return response()->json(["Category" => $categories],
-        JsonResponse::HTTP_OK);
-
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param \App\Categories $categories
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-        try {
-            $categories=Categories::find($id);
-            if(!$categories)
-                throw new NotFoundResourceException("Unit Measure $id Not Found");
-            $categories->delete();
-        } catch (\Exception $exception) {
-
-            return response()->json(
-                ["errors" => $exception->getMessage()],
-                JsonResponse::HTTP_BAD_REQUEST);
-        }
-        return response()->json(["Category" => null],JsonResponse::HTTP_NO_CONTENT);
-    }
 }
--- app/Http/Controllers/UnitMeasureController.php
+++ app/Http/Controllers/UnitMeasureController.php
@@ -4,14 +4,22 @@ namespace App\Http\Controllers;
 
 
 use App\UnitMeasure;
+use App\Utils\BaseController;
 use Exception;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Validation\ValidationException;
 use Symfony\Component\Translation\Exception\NotFoundResourceException;
 
-class UnitMeasureController extends Controller
+class UnitMeasureController extends BaseController
 {
+
+    public function __construct()
+    {
+        parent::__construct(UnitMeasure::class);
+    }
+
+
     /**
      * Display a listing of the resource.
      *
@@ -23,75 +31,4 @@ class UnitMeasureController extends Controller
         return view("product.indexUnitMeasure", compact("unitMeasure"));
     }
 
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param \Illuminate\Http\Request $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-
-        try {
-            $unitMeasure = new UnitMeasure();
-            $unitMeasure->validateAndFill($request->all());
-            $unitMeasure->save();
-        } catch (ValidationException $exception) {
-
-            return response()->json(
-                ["errors" => $exception->validator->getMessageBag()],
-                JsonResponse::HTTP_UNPROCESSABLE_ENTITY);
-        }
-        return response()->json(["UnitMeasure" => $unitMeasure],
-            JsonResponse::HTTP_CREATED);
-
-
-    }
-
-
-    public function update(Request $request, $id)
-    {
-        try {
-            $unitMeasureSaved=UnitMeasure::find($id);
-            if(!$unitMeasureSaved)
-                throw new NotFoundResourceException("Unit Measure $id Not Found");
-            $unitMeasureSaved->validateAndFill($request->all());
-            $unitMeasureSaved->save();
-        } catch (ValidationException $exception) {
-
-            return response()->json(
-                ["errors" => $exception->validator->getMessageBag()],
-                JsonResponse::HTTP_UNPROCESSABLE_ENTITY);
-        }
-        catch (NotFoundResourceException $notFoundE) {
-
-            return response()->json(
-                ["errors" => $notFoundE->getMessage()],
-                JsonResponse::HTTP_NOT_FOUND);
-        }
-        return response()->json(["UnitMeasure" => $unitMeasureSaved],
-            JsonResponse::HTTP_OK);
-
-    }
-
-
-
-    public function destroy($id)
-    {
-        //
-        try {
-            $unitMeasure=UnitMeasure::find($id);
-            if(!$unitMeasure)
-                throw new NotFoundResourceException("Unit Measure $id Not Found");
-            $unitMeasure->delete();
-        } catch (Exception $exception) {
-
-            return response()->json(
-                ["errors" => $exception->getMessage()],
-                JsonResponse::HTTP_BAD_REQUEST);
-        }
-        return response("FFF")->json(["UnitMeasure" => "Destroy"],
-            JsonResponse::HTTP_NO_CONTENT);
-    }
 }
