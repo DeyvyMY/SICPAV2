471f506b44b7c4d3de1af8b5714c02b1dc9a6365
DeyvyMY <odr5_deivioscar@hotmail.com>
2019-10-16 00:16:02 -0500

    voucher works

--- app/Http/Controllers/VouchersController.php
+++ app/Http/Controllers/VouchersController.php
@@ -3,37 +3,32 @@
 namespace App\Http\Controllers;
 
 use App\Entidad;
+use App\TipoGastos;
 use App\Vendors;
 use App\Vouchers;
 use App\VoucherType;
 use Exception;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Validation\ValidationException;
-use Illuminate\Http\JsonResponse;
 
 class VouchersController extends Controller
 {
 
     public function index()
     {
-        $vouchers = TipoGastos::all();
-        $vendors=Vendors::all(["name","id"]);
-        $entidades=Entidad::all(["ent_id","ent_rz"]);
-        $types=VoucherType::all(["id","description"]);
-        return view("gastos.tipoGastos", ["vouchers"=>$vouchers,"vendors"=>$vendors,"entities"=>$entidades,"types"=>$types]);
+        $tipoGastos = TipoGastos::all();
+        $vendors    = Vendors::all(["name", "id"]);
+        $entidades  = Entidad::all(["ent_id", "ent_rz"]);
+        $types      = VoucherType::all(["id", "description"]);
+        return view("gastos.tipogastos", ["tipoGastos" => $tipoGastos, "vendors" => $vendors, "entities" => $entidades, "types" => $types]);
     }
-
-
-
-
-
-
 
     public function store(Request $request)
     {
 
         try {
-            $buyVoucher= new Vouchers();
+            $buyVoucher = new Vouchers();
             $buyVoucher->saveBuy($request->all());
 
         } catch (ValidationException $exception) {
@@ -41,14 +36,13 @@ class VouchersController extends Controller
             return response()->json(
                 ["errors" => $exception->validator->getMessageBag()],
                 JsonResponse::HTTP_UNPROCESSABLE_ENTITY);
-        }catch (Exception $e){
+        } catch (Exception $e) {
             return response()->json(
                 ["errors" => $e->getMessage()],
                 JsonResponse::HTTP_INTERNAL_SERVER_ERROR);
         }
         return response()->json(["BuyVoucher" => $buyVoucher],
             JsonResponse::HTTP_CREATED);
-
 
     }
 }
