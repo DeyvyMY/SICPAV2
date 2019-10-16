a3b232e9ddf1a91093512642db47bf36c36b75be
DeyvyMY <odr5_deivioscar@hotmail.com>
2019-10-16 00:37:42 -0500

    migration model and controller for operations & type operations

---
+++ app/Http/Controllers/OperationTypeController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\OperationType;
+use Illuminate\Http\Request;
+
+class OperationTypeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\OperationType  $operationType
+     * @return \Illuminate\Http\Response
+     */
+    public function show(OperationType $operationType)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\OperationType  $operationType
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(OperationType $operationType)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\OperationType  $operationType
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, OperationType $operationType)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\OperationType  $operationType
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(OperationType $operationType)
+    {
+        //
+    }
+}
---
+++ database/migrations/2019_10_16_053121_create_operations_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOperationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('operations', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->integer('operation_type_id')->unsigned();
+            $table->foreign('operation_type_id')->references('id')->on('operation_types');
+
+            $table->integer('voucher_id')->unsigned();
+            $table->foreign('voucher_id')->references('id')->on('vouchers');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('operations');
+    }
+}
