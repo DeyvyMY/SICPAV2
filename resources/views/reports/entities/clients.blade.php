e6690f0f657d407c9e1c118228974b5e0b567b52
DeyvyMY <odr5_deivioscar@hotmail.com>
2019-10-30 02:00:41 -0500

    Report Client View

--- resources/views/reports/entities/clients.blade.php
+++ resources/views/reports/entities/clients.blade.php
@@ -8,19 +8,46 @@
 	<body>
 		<div>
 			<table class="table">
-				<tr>
-					<th>RUC ó DNI</th>
-					<th>Razón Social</th>
-					<th>Dirección</th>
-					<th>Departamento</th>
-					<th>Ciudad</th>
-					<th>Provincia</th>
-					<th>Distrito</th>
-					<th>Teléfono</th>
-					<th>Contacto</th>
-					<th>Correo Electronico</th>
-					<th>Teléfono de Contacto</th>
+				<tr style="font-family: consolas;">
+					<th colspan="5">
+						<img src="Images/logo.jpg">
+					</th>
 				</tr>
+				<tr style="font-family: consolas; font-size: 50px;">
+					<th height="40">
+						REPORTE DE CLIENTES
+					</th>
+				</tr>
+				<tr style="font-family: consolas; background: #4BB543; color: #fff">
+					<th width="20" height="25">RUC ó DNI</th>
+					<th width="20" height="25">Razón Social</th>
+					<th width="20" height="25">Dirección</th>
+					<th width="20" height="25">Departamento</th>
+					<th width="20" height="25">Ciudad</th>
+					<th width="20" height="25">Provincia</th>
+					<th width="20" height="25">Distrito</th>
+					<th width="20" height="25">Teléfono</th>
+					<th width="20" height="25">Contacto</th>
+					<th width="20" height="25">Correo Electronico</th>
+					<th width="20" height="25">Teléfono de Contacto</th>
+				</tr>
+				@if(sizeof($clients) > 0)
+					@foreach ($clients as $client)
+						<tr style="font-family: consolas;">
+							<th width="20" height="25">{{$client->ent_ruc}}</th>
+							<th width="20" height="25">{{$client->ent_rz}}</th>
+							<th width="20" height="25">{{$client->ent_dir}}</th>
+							<th width="20" height="25">{{$client->ent_dpto}}</th>
+							<th width="20" height="25">{{$client->ent_ciu}}</th>
+							<th width="20" height="25">{{$client->ent_prov}}</th>
+							<th width="20" height="25">Distrito</th>
+							<th width="20" height="25">Teléfono</th>
+							<th width="20" height="25">{{$client->ent_cont}}</th>
+							<th width="20" height="25">{{$client->ent_correo}}</th>
+							<th width="20" height="25">{{$client->ent_ctel}}</th>
+					</tr>
+					@endforeach
+				@endif
 			</table>
 		</div>
   </body>
