cda78ac027d3c2926956291eec85bb2876c1c9b2
DeyvyMY <odr5_deivioscar@hotmail.com>
2019-11-12 22:26:25 -0500

    report whit nothing

--- resources/views/reports/entities/clients.blade.php
+++ resources/views/reports/entities/clients.blade.php
@@ -7,31 +7,7 @@
 	</head>
 	<body>
 		<div>
-			<table class="table">
-				<tr style="font-family: consolas;">
-					<th colspan="5">
-						<img src="Images/logo.jpg">
-					</th>
-				</tr>
-				<tr style="font-family: consolas; font-size: 50px;">
-					<th height="40">
-						REPORTE DE CLIENTES
-					</th>
-				</tr>
-				<tr style="font-family: consolas; background: #4BB543; color: #fff">
-					<th width="20" height="25">RUC ó DNI</th>
-					<th width="20" height="25">Razón Social</th>
-					<th width="20" height="25">Dirección</th>
-					<th width="20" height="25">Departamento</th>
-					<th width="20" height="25">Ciudad</th>
-					<th width="20" height="25">Provincia</th>
-					<th width="20" height="25">Distrito</th>
-					<th width="20" height="25">Teléfono</th>
-					<th width="20" height="25">Contacto</th>
-					<th width="20" height="25">Correo Electronico</th>
-					<th width="20" height="25">Teléfono de Contacto</th>
-				</tr>
-			</table>
+			
 		</div>
   </body>
 </html>
